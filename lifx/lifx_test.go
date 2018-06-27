0b8ffd62fcd0879afb612bbad2af33db553c3a39
Marta Paciorkowska <marta@paciorkowska.pl>
2018-06-27 19:41:39 +0200

    [WIP] tests

--- lifx/lifx_test.go
+++ lifx/lifx_test.go
@@ -2,14 +2,19 @@ package lifx
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
-var LifxMessage []byte = { 
-'\x31', '\x00', '\x00', '\x34', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x66', '\x00', '\x00', '\x00', '\x00', '\x55', '\x55', '\xFF', '\xFF', '\xFF', '\xFF', '\xAC', '\x0D', '\x00', '\x04', '\x00', '\x00'
+var LightMessage = []byte{
+	'\x31', '\x00', '\x00', '\x34', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00',
+	'\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00',
+	'\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00',
+	'\x00', '\x00', '\x66', '\x00', '\x00', '\x00', '\x00', '\x55', '\x55', '\xFF',
+	'\xFF', '\xFF', '\xFF', '\xAC', '\x0D', '\x00', '\x04', '\x00', '\x00',
 }
 
-func TestAThing(t *testing.T) {
-	assrt := assert.New(t)
+// assuming the server receives a message like LifxMessage above,
+// the Decode function returns a Payload struct.
+func TestDecode(t *testing.T) {
+	// assrt := assert.New(t)
+
 }
