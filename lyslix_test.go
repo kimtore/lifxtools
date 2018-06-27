87cdcbdd28a3c17c20fa92d05e020c8047fdd169
Marta Paciorkowska <marta@paciorkowska.pl>
2018-06-27 19:14:13 +0200

    [WIP] Tests

---
+++ lyslix_test.go
@@ -0,0 +1,15 @@
+package lysix
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var LifxMessage []byte
+
+// "31 00 00 34 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 66 00 00 00 00 55 55 FF FF FF FF AC 0D 00 04 00 00"
+
+func TestAThing(t *testing.T) {
+	assrt := assert.New(t)
+}
