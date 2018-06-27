778571a20c69a4be21db05e9852d7f562b5e33be
Kim Tore Jensen <kim@incendio.no>
2018-06-27 19:42:30 +0200

    Add light message data types.

---
+++ lifx/light.go
@@ -0,0 +1,31 @@
+package lifx
+
+// From https://lan.developer.lifx.com/docs/light-messages
+
+// HSBK is used to represent the color and color temperature of a light.
+//
+// The color is represented as an HSB (Hue, Saturation, Brightness) value.
+//
+// The color temperature is represented in K (Kelvin) and is used to adjust the warmness / coolness of a white light, which is most obvious when saturation is close zero.
+//
+// Hue: range 0 to 65535
+// Saturation: range 0 to 65535
+// Brightness: range 0 to 65535
+// Kelvin: range 2500° (warm) to 9000° (cool)
+type HBSK struct {
+	Hue        uint16
+	Saturation uint16
+	Brightness uint16
+	Kelvin     uint16
+}
+
+// Sent by a client to change the light state.
+//
+// The duration is the color transition time in milliseconds.
+//
+// If the Frame Address res_required field is set to one (1) then the device will transmit a State message.
+type SetColorMessage struct {
+	Reserved uint8
+	Color    HBSK
+	Duration uint32
+}
