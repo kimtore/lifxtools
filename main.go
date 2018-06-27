c4879037eed8f5fa86d6c41b33560cb77103e638
Kim Tore Jensen <kim@incendio.no>
2018-06-27 20:08:37 +0200

    add some UDP server code

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+const LISTEN_ADDR string = "0.0.0.0"
+const DEFAULT_PORT string = "56700"
+
+func getUdpAddr() (*net.UDPAddr, error) {
+	listenAddr := LISTEN_ADDR
+	portNumber := os.Getenv("PORT")
+	if len(portNumber) == 0 {
+		portNumber = DEFAULT_PORT
+	}
+	strAddr := fmt.Sprintf("%s:%s", listenAddr, portNumber)
+	return net.ResolveUDPAddr("udp", strAddr)
+}
 
 func main() {
 	fmt.Println("lyslix 1.0")
+
+	addr, err := getUdpAddr()
+	if err != nil {
+		return
+	}
+
+	_ = addr
 }
