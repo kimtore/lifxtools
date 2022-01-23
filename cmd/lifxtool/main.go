21dcceaaf525134fcef34f08bbf9cf2048d22f7a
Kim Tore Jensen <kimtjen@gmail.com>
2022-01-23 02:02:38 +0100

    proof of concept

--- cmd/lifxtool/main.go
+++ cmd/lifxtool/main.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/dorkowscy/lyslix/lifx"
+)
+
+func main() {
+	host := flag.String("host", "", "lifx bulb hostname or ip address")
+	port := flag.Uint("port", 56700, "lifx bulb port")
+	flag.Parse()
+
+	addr := fmt.Sprintf("%s:%d", *host, *port)
+	client, err := lifx.NewClient(addr)
+	if err != nil {
+		panic(err)
+	}
+
+	defer client.Close()
+
+	framerate := time.Second / 10
+
+	rand.Seed(time.Now().UnixNano())
+
+	for hue := uint16(0); hue < lifx.MaxHue; hue += 50 {
+		color := lifx.HBSK{
+			Hue:        hue,
+			Saturation: lifx.FullSaturation,
+			Brightness: lifx.FullBrightness,
+		}
+		fmt.Println(color)
+		client.SetColor(color, 0)
+		time.Sleep(framerate)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/dorkowscy/lifxtool
 
 go 1.16
+
+require github.com/dorkowscy/lyslix v0.0.0-20220122221746-8bd118618c2f
+
+replace github.com/dorkowscy/lyslix => ../lyslix
