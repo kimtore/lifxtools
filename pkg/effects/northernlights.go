6711aabb4a1d0bf9bd9eced76c7a07e14544eedc
Kim Tore Jensen <kimtjen@gmail.com>
2022-01-31 14:16:42 +0100

    northern lights: smoother motion

--- pkg/effects/northernlights.go
+++ pkg/effects/northernlights.go
@@ -3,7 +3,9 @@ package effects
 import (
 	"math/rand"
 
+	"github.com/dorkowscy/lifxtool/pkg/textutil"
 	"github.com/lucasb-eyer/go-colorful"
+	log "github.com/sirupsen/logrus"
 )
 
 type NorthernLights struct {
@@ -14,6 +16,8 @@ type NorthernLights struct {
 	Brighten  float64 `json:"brighten"`  // how much to brighten new dots
 	Saturate  float64 `json:"saturate"`  // how much to saturate new dots
 	Color     Color   `json:"color"`
+	fades     []float64
+	dots      []colorful.Color
 }
 
 func init() {
@@ -22,9 +26,31 @@ func init() {
 
 func (e *NorthernLights) Init(pixels []colorful.Color) {
 	fill(pixels, e.Color.Color)
+	e.fades = make([]float64, len(pixels))
+	e.dots = make([]colorful.Color, len(pixels))
 }
 
 func (e *NorthernLights) Draw(pixels []colorful.Color) {
+	for i := range pixels {
+		if rand.Float64() < e.Intensity {
+			// Generate a new pixel within configured range
+			h, c, l := e.Color.Hcl()
+			h += rnd() * e.Diversity
+			c += e.Saturate
+			l += e.Brighten
+			e.dots[i] = colorful.Hcl(h, c, l)
+			e.fades[i] = 1
+			log.Debugf("Generating new pixel at position %d: %s", i, textutil.SprintfHCL(e.dots[i]))
+		}
+		if e.fades[i] > 0 {
+			pixels[i] = e.Color.BlendHcl(e.dots[i], e.fades[i])
+			e.fades[i] -= e.Fade
+		} else {
+			pixels[i] = e.Color.Color
+		}
+	}
+}
+func (e *NorthernLights) DrawLegacy(pixels []colorful.Color) {
 	for i := range pixels {
 		if e.Intensity < rand.Float64() {
 			// Fade out existing pixels towards the base color
