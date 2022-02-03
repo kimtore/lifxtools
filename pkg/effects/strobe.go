7ada5959f1f84dc1e429479548c8ec05fbdcdb51
Kim Tore Jensen <kimtjen@gmail.com>
2022-02-03 01:00:46 +0100

    strobe: render back and forth

--- pkg/effects/strobe.go
+++ pkg/effects/strobe.go
@@ -6,13 +6,12 @@ import (
 
 // Strobe shoots pixels across the canvas, fading them out in the process.
 type Strobe struct {
-	//Chroma    float64 `json:"chroma"`    // Saturation of colors, HCL space, from 0.0-1.0
-	//Luminance float64 `json:"luminance"` // Brightness of colors, HCL space, from 0.0-1.0
-	Color     Color   `json:"color"`
-	Length    int     `json:"length"`
+	Color     Color   `json:"color"`     // Color of stroboscope
+	Length    int     `json:"length"`    // Length of tail
 	Intensity float64 `json:"intensity"` // Chance to generate new pixel
 	Fade      float64 `json:"fade"`      // Fade out per iteration
-	remaining int
+
+	direction int
 	pos       int
 	fades     []float64
 	dots      []colorful.Color
@@ -25,28 +24,36 @@ func init() {
 func (e *Strobe) Init(pixels []colorful.Color) {
 	e.fades = make([]float64, len(pixels))
 	e.dots = make([]colorful.Color, len(pixels))
+	e.direction = 1
 	e.Draw(pixels)
 }
 
 func (e *Strobe) Draw(pixels []colorful.Color) {
 	h, _, _ := e.Color.Hcl()
+
+	// Reference black can't be entirely black, since this will blend to red.
 	black := colorful.Hcl(h, 0.01, 0)
 	for i := range pixels {
-		if i == e.pos && e.remaining > 0 {
+		if i == e.pos {
 			// Strobe flash in progress, draw next pixel
 			e.dots[i] = e.Color.Color
 			e.fades[i] = 1
 			pixels[i] = e.dots[i]
-			e.remaining--
 		} else if e.fades[i] > 0 {
 			// Fade out a pixel that was generated earlier
 			pixels[i] = black.BlendHcl(e.dots[i], e.fades[i])
 			e.fades[i] -= 1.0 / float64(e.Length)
-		} else if e.remaining == 0 && randomNonNegative() < e.Intensity {
-			e.remaining = e.Length
 		} else {
 			pixels[i] = black
 		}
 	}
-	e.pos = (e.pos + 1) % len(pixels)
+
+	e.pos += e.direction
+	if e.pos >= len(pixels) {
+		e.pos = len(pixels) - 1
+		e.direction = -1
+	} else if e.pos < 0 {
+		e.pos = 0
+		e.direction = 1
+	}
 }
